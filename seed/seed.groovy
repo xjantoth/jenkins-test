c5724c291901a19dd350728ca1e19970192f1e53
Jan Toth <jan.toth@vacuumlabs.com>
2022-03-10 17:01:43 +0100

    add seed fix

--- seed/seed.groovy
+++ seed/seed.groovy
@@ -1,21 +1,23 @@
 job("${SEED_PROJECT}-${SEED_BRANCH}-build") {
    description "Building the ${BRANCH} branch."
-   parameters {
-      stringParam('COMMIT', 'HEAD', 'Commit to build')
-   }
+      parameters {
+         stringParam('COMMIT', 'HEAD', 'Commit to build')
+      }
    scm {
       git {
          remote {
-             url PROJECT_SCM_URL
-             branch '${COMMIT}'
+            url PROJECT_SCM_URL
+               branch '${COMMIT}'
          }
          extensions {
-             wipeOutWorkspace()
-             localBranch BRANCH
+            wipeOutWorkspace()
+               localBranch BRANCH
          }
       }
    }
-   steps {
-      shell "Look! I'm building ${BRANCH}!"
+   definition {
+      cpsScm {
+         scriptPath("Jenkinsfile")
+      }
    }
 }
