d8d2cf1133050dd74688e46cea5d80f9367a2c64
Jan Toth <jan.toth@vacuumlabs.com>
2022-03-14 10:09:28 +0100

    adding credentials - correct one ccccc

--- seed/seed.groovy
+++ seed/seed.groovy
@@ -18,6 +18,9 @@ freeStyleJob("${SEED_PROJECT}-${SEED_BRANCH}-build") {
 
 pipelineJob("${SEED_PROJECT}-${SEED_BRANCH}-builddeploy") {
     description "Building and deploying the ${BRANCH} branch."
+// parameters {
+// stringParam('PROJECT_SCM_URL', "${PROJECT_SCM_URL}", 'The Bitbucket project name including the workspace.')
+// }
 
     // because stash notifier will not work
     triggers {
@@ -34,8 +37,8 @@ pipelineJob("${SEED_PROJECT}-${SEED_BRANCH}-builddeploy") {
             scm {
                 git("${PROJECT_SCM_URL}", "${BRANCH}") { node ->
                     node / 'userRemoteConfigs' / 'hudson.plugins.git.UserRemoteConfig' {
-                        credentialsId('SSH_KEY_SECURITY')
-                            url('${PROJECT_SCM_URL}')
+                        credentialsId("SSH_KEY_SECURITY")
+                            url("${PROJECT_SCM_URL}")
                     }
                 }
                 scriptPath("Jenkinsfile")
